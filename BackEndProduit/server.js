4ba094315cb24e13662fbcd7faff76b4c5b90a48
Seriane <hdadech.nizar@gmail.com>
2017-01-02 13:58:32 +0100

    adding comments

--- BackEndProduit/server.js
+++ BackEndProduit/server.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var app = express();
 
+//add (index) to the file if already exists
 function name_change(filen,index){
 
 	var exists = fs.existsSync(__dirname+'/upload/'+filen+'('+index+')');
@@ -16,6 +17,7 @@ function name_change(filen,index){
 
 	return filen;
 }
+//setting the upload directory + change name of file if it already exists
 var storage = multer.diskStorage(
 	{
 		destination:'./upload/',
@@ -38,17 +40,21 @@ var storage = multer.diskStorage(
 	}
 
 );
+//initialise multer module
 var upload_file = multer({storage:storage});
 
+//define the route of /
 app.get('/',function(req,res) {
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+//send file to upload and send message to the user.
 app.post('/upload',upload_file.single('fileupload'),function(req,res){
 res.end(req.file.filename+' was succesfully uploaded!');
 
 });
 
+//if path is not available
 app.use(function(req,res,next) {
 	res.status(404).send("NOPE!");
 });
